3548bd9785e3e030eecb1d35f6c12d2b09511e1d
violet-devsec <titto.tvm@hotmail.com>
2022-09-11 19:44:48 +0530

    aslr bypass-2: vuln program updated

--- aslr-bypass/case-2/aslr_ret2plt.c
+++ aslr-bypass/case-2/aslr_ret2plt.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+void show_date() {
+    system("/bin/date");
+}
+
+void greet_me() {
+    char name[200];
+    printf("Enter your name:");
+    gets(name);
+    printf("%s ! it is you again !!! oh my ghosh", name);
+}
 
 int main(int argc, char *argv[]) {
     show_date();

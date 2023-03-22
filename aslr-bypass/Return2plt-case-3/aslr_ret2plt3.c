bd4b17f6c36012cc4b286827eadcd27fc844c8b7
violet-devsec <titto.tvm@hotmail.com>
2023-03-22 12:46:14 +0530

    Vuln C prgm updated

--- aslr-bypass/Return2plt-case-3/aslr_ret2plt3.c
+++ aslr-bypass/Return2plt-case-3/aslr_ret2plt3.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+void greet_me()
+{
+    char name[200];
+    show_date();
+    printf("Enter your name:");
+    gets(name);
+    printf("hi %s !\n",name);
+  
+}
+
 int main(int argc, char *argv[])
 {
     greet_me();

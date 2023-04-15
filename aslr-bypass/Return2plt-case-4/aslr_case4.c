f437c3e9ba4308924a6521c2c834f08b2398330b
violet-devsec <titto.tvm@hotmail.com>
2023-04-15 15:00:35 +0530

    aslr case3: vuln pgm updated

--- aslr-bypass/Return2plt-case-4/aslr_case4.c
+++ aslr-bypass/Return2plt-case-4/aslr_case4.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+void greet_me()
+{
+        char name[200];
+        printf("Enter your name:");
+        gets(name);
+        printf("Hi %s !\n", name);
+}
+
 int main(int argc, char *argv[])
 {
+    greet_me();
     return 0;
 }

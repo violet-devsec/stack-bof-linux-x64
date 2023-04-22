6977937c76ea692e07443e8d13ab9099e32d5688
violet-devsec <titto.tvm@hotmail.com>
2023-04-22 22:15:38 +0530

    aslr case4: vuln pgm updated

--- aslr-bypass/Return2plt-case-4/aslr_case4.c
+++ aslr-bypass/Return2plt-case-4/aslr_case4.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+__asm__(".global func          \n\t"
+        ".type func, @function \n\t"
+        "func:                 \n\t"
+        " .cfi_startproc       \n\t"
+        " sub %rbp, (%rdi)     \n\t"
+        " ret                  \n\t"
+        " .cfi_endproc             ");
+
+char *dummy = "sh";
+
 void greet_me()
 {
         char name[200];
